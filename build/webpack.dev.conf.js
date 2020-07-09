62433146132d5cba7b081b8f8cad27e81d7678f5
ningzhenyu <zhenyu_ning@qingsongchou.com>
2020-07-09 16:43:52 +0800

    webpack-dev-server

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,15 +3,16 @@ var merge = require('webpack-merge')
 
 const baseConfig = require('./webpack.base.conf');
 
-module.exports = merge(baseConfig), {
+module.exports = merge(baseConfig, {
   devServer: {
     host: 'localhost',
-    port: '8005',
+    port: 8005,
     https: true,
     open: true,
     hot: true,
+    quiet: true,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin() //热更新
   ],
-}
+})
