5d8ba71f05ea3bbefe7c84ef7b9945b621ed9b28
ningzhenyu <zhenyu_ning@qingsongchou.com>
2020-07-21 16:32:50 +0800

    跨域

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,12 +1,16 @@
-const koaBody = require('koa-body');
+const koaBody = require('koa-body');」
+
+const allowOrigin = []
 
 function middleware(app) {
   // 跨域
   app.use(async (ctx, next) => {
-    ctx.set("Access-Control-Allow-Origin", "http://localhost:8005");
-    ctx.set('Access-Control-Allow-Methods', 'PUT,DELETE,POST,GET');
-    ctx.set('Access-Control-Allow-Headers', 'Accept,Origin,XRequestedWith,Content-Type,LastModified');
-    ctx.set('Access-Control-Allow-Credentials', true);
+    if(true || allowOrigin.indexOf(ctx.headers.origin) > -1){
+      ctx.set("Access-Control-Allow-Origin", ctx.headers.origin);
+      ctx.set('Access-Control-Allow-Methods', 'PUT,DELETE,POST,GET');
+      ctx.set('Access-Control-Allow-Headers', 'Accept,Origin,XRequestedWith,Content-Type,LastModified');
+      ctx.set('Access-Control-Allow-Credentials', true);
+    }
     await next();
   });
   // 解析 post 请求的 body
